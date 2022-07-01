6e22b59c9660f3f94b06ac8c36c820ab7c515d41
muhammadramadhann <ramadhannkurniawan@gmail.com>
2022-07-01 21:09:08 +0700

    Optimize Layout

--- resources/views/components/recipe-item.blade.php
+++ resources/views/components/recipe-item.blade.php
@@ -1,17 +1,13 @@
-<div class="col-xl-4 col-lg-6 col-12">
-    <div class="card h-100 mb-md-5 mb-4">
-        <a href="{{ route('detail-recipe', $slug) }}" class="text-decoration-none">
+<div class="col-xl-4 col-lg-6 col-12 d-flex align-items-stretch">
+    <div class="card flex-fill mb-md-5 mb-4">
+        <a href="{{ route('detail-recipe', $slug) }}" class="text-decoration-none d-block">
             <img src="images/recipe_images/{{ $image }}" class="card-img-top" alt="gambar-resep">
-            <div class="card-body pb-0 px-0">
+            <div class="card-body px-0">
                 <h6 class="card-subtitle mb-2 text-muted fw-normal"><span>{{ $likes }}</span> Orang Menyukai ini</h6>
                 <h4 class="card-title text-resepku">{{ $title }}</h4>
+                <p class="card-text fw-normal text-black mb-3">{{ $excerpt }}</p>
+                <a href="{{ route('recipe.like', $id) }}" class="btn btn-secondary py-2 w-100">Suka</a>
             </div>
         </a>
-        <div class="card-body py-0 px-0">
-            <p class="card-text fw-normal text-black">{{ $excerpt }}</p>
-        </div>
-        <div class="card-body pb-0 px-0">
-            <a href="{{ route('recipe.like', $id) }}" class="btn btn-secondary py-2 w-100">Suka</a>
-        </div>
     </div>
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,16 @@
         <x-recipe-empty/>
     @else
         <h4 class="mb-4">Resep Terbaru</h4>
-        <section class="recipe-item row justify-content-between align-items-start">
+        <section class="recipe-item row justify-content-between">
             @foreach ($recipes as $recipe)
                 <x-recipe-item 
                     id="{{ $recipe->id }}"
                     slug="{{ $recipe->slug }}" 
-                    likes="{{ $likes->where('recipe_id', $recipe->id)->count() }}" 
+                    likes="{{ ($likes->where('recipe_id', $recipe->id)->count()) }}"
                     title="{{ $recipe->title }}" 
                     image="{{ $recipe->image }}" 
-                    excerpt="{{ $recipe->excerpt }}" />
+                    excerpt="{{ $recipe->excerpt }}" 
+                />
             @endforeach
         </section>
     @endif
