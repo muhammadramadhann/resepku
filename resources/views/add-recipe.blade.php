045d97eabdd1facf9b083a300002ab2c7bd778f0
muhammadramadhann <ramadhannkurniawan@gmail.com>
2022-07-01 19:44:31 +0700

    Fix Validator and Error Input

--- resources/views/add-recipe.blade.php
+++ resources/views/add-recipe.blade.php
@@ -26,12 +26,12 @@
                 <input type="text" class="form-control" id="ingredients" name="add_more_ingredients[0][ingredients]" placeholder="3 siung bawang merah" value="{{ old('add_more_ingredients.0.ingredients') }}">
                 <input type="text" class="form-control mt-2" id="ingredients" name="add_more_ingredients[1][ingredients]" placeholder="3 siung bawang merah" value="{{ old('add_more_ingredients.1.ingredients') }}">
             </div>
-            <button type="button" name="more-ingredients" id="more-ingredients" class="btn btn-link text-resepku mt-1">+ Item Bahan</button>
             @error('add_more_ingredients.*.ingredients')
                 <div class="text-danger mt-2">
                     {{ $message }}
                 </div>
             @enderror
+            <button type="button" name="more-ingredients" id="more-ingredients" class="btn btn-link text-resepku mt-1">+ Item Bahan</button>
         </div>
         <div class="mb-3">
             <label for="cooking_steps" class="form-label fw-bolder">Langkah Pembuatan</label>
@@ -39,12 +39,12 @@
                 <input type="text" class="form-control" id="cooking_steps" name="add_more_cooking_steps[0][cooking_steps]" placeholder="Iris bawang menjadi potongan kecil" value="{{ old('add_more_cooking_steps.0.cooking_steps') }}">
                 <input type="text" class="form-control mt-2" id="cooking_steps" name="add_more_cooking_steps[1][cooking_steps]" placeholder="Iris bawang menjadi potongan kecil" value="{{ old('add_more_cooking_steps.1.cooking_steps') }}">
             </div>
-            <button type="button" name="add-cooking-steps" id="more-cooking-steps" class="btn btn-link text-resepku mt-1">+ Item Langkah</button>
             @error('add_more_cooking_steps.*.cooking_steps')
                 <div class="text-danger mt-2">
                     {{ $message }}
                 </div>
             @enderror
+            <button type="button" name="add-cooking-steps" id="more-cooking-steps" class="btn btn-link text-resepku mt-1">+ Item Langkah</button>
         </div>
         <div class="mb-4">
             <label for="image" class="form-label fw-bolder">Upload Foto Masakan</label>
